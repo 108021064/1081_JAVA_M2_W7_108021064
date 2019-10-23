f1330ec5844ea632f2a3f726778aae057c62f4c3
Yung-Chen Chou <yungchen@asia.edu.tw>
2019-10-23 15:55:29 +0800

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
@@ -9,11 +10,25 @@ public class Main {
             sum = sum+i;
         }
         System.out.println(sum);*/
-        int sum = 0;
+        /*int sum = 0;
         int a = scn.nextInt();
         for (int i = 0;i<=a ; i=i+3){
             sum = sum+i;
         }
-        System.out.println(sum);
+        System.out.println(sum);*/
+        int n = scn.nextInt();
+        int m = n / 2;
+        boolean flag = true;
+        for (int i = 2; i <= m; i++) {
+            if (n % i == 0) {
+                flag = false;
+                break;
+            }
+        }
+        if (flag) {
+            System.out.println("yeeeeeeeeeeeeeeeeeeeeeeeees");
+        } else {
+            System.out.println("noooooooooooooooooooooooooo");
+        }
     }
 }
