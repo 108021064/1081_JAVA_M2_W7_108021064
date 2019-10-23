6697a83bff31c6adc92d6952f25e1d392607b732
Yung-Chen Chou <yungchen@asia.edu.tw>
2019-10-23 14:54:56 +0800

    commit

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        int sum = 0;
+        /*int sum = 0;
         for (int i=1;i<=100;i++){
+            sum = sum+i;
+        }
+        System.out.println(sum);*/
+        int sum = 0;
+        int a = scn.nextInt();
+        for (int i = 0;i<=a ; i=i+3){
             sum = sum+i;
         }
         System.out.println(sum);
