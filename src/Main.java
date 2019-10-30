06debd12126a4de44069cfa0e7a154cb58155bbb
Yung-Chen Chou <yungchen@asia.edu.tw>
2019-10-30 16:00:40 +0800

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,21 @@
-import java.sql.SQLOutput;
-import java.util.Scanner;
-
+import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        /*int sum = 0;
-        for (int i=1;i<=100;i++){
-            sum = sum+i;
-        }
-        System.out.println(sum);*/
-        /*int sum = 0;
-        int a = scn.nextInt();
-        for (int i = 0;i<=a ; i=i+3){
-            sum = sum+i;
-        }
-        System.out.println(sum);*/
-        /*int n = scn.nextInt();
-        int m = n / 2;
-        boolean flag = true;
-        for (int i = 2; i <= m; i++) {
-            if (n % i == 0) {
-                flag = false;
-                break;
-            }
-        }
-        if (flag) {
-            System.out.println("yeeeeeeeeeeeeeeeeeeeeeeeees");
-        } else {
-            System.out.println("noooooooooooooooooooooooooo");
-        }*/
         int n = scn.nextInt();
-        int sum = 0;
-        for (int i=1;i<=n;i++){
-            sum=sum+i*(i+1);
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = n; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i * 2 - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+
+
         }
-        System.out.println(sum);
+
     }
 }
